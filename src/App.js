b59590f70dfbfcbf9dcb7829ea50a3527a96d920
Won <hwon466@gmail.com>
2023-08-31 11:11:49 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todoList === "") {
+      return;
+    }
+    setTodos((currentTodos) => [todoList, ...currentTodos]);
+    setTodoList("");
+  };
+  const onChange = (e) => {
+    setTodoList(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="입력하세요"
+          onChange={onChange}
+          value={todoList}
+        />
+        <button>등록하기</button>
+        <h1 className="todoListTitle">Todo List</h1>
+      </form>
+      <div className="todoBoxContainer">
+        {todos.map((item, index) => (
+          <li className="todoBox">{item}</li>
+        ))}
+      </div>
     </div>
   );
 }
